05522ae49d40d74eb014e1e65f199a1b249ac7eb
LuckDev22 <103015314+LuckDev22@users.noreply.github.com>
2023-03-07 12:39:24 -0300

    feat: components add Input TextField miu

--- Hyperion/src/components/Forms/Input/index.tsx
+++ Hyperion/src/components/Forms/Input/index.tsx
@@ -1,5 +1,6 @@
 import { TextField } from "@mui/material";
 import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+import { StyledFieldset } from "./styled";
 
 interface IInputProps {
   label: string;
@@ -10,9 +11,9 @@ interface IInputProps {
 
 export const Input = ({ label, type, error, register }: IInputProps) => {
   return (
-    <fieldset>
+    <StyledFieldset>
       <TextField label={label} type={type} {...register} />
       {error ? <p> {error.message}</p> : null}
-    </fieldset>
+    </StyledFieldset>
   );
 };
