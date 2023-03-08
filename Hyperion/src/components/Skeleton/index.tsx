99379229860d1bdd4d315d5b9dc425dfe9dd5d63
Mateus Carius <matt_cariusp@hotmail.com>
2023-03-08 15:20:43 -0300

    added:Menu Skeleton Loading-Spinner Drawer & Avatar

---
+++ Hyperion/src/components/Skeleton/index.tsx
@@ -0,0 +1,9 @@
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+
+export const DefaultSkeleton = () => {
+  <>
+    <Skeleton height="20px" />
+    <Skeleton height="20px" />
+    <Skeleton height="20px" />
+  </>;
+};
