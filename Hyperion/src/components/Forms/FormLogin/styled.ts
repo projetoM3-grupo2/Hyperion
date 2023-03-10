28feefe4a2fbadaedf0c0df174bb549eb070746c
Vitor Hugo Reis Moreira <107319103+theonlyvitor@users.noreply.github.com>
2023-03-10 11:59:16 -0400

    Feat: Add Overlay on login-background

--- Hyperion/src/components/Forms/FormLogin/styled.ts
+++ Hyperion/src/components/Forms/FormLogin/styled.ts
@@ -3,9 +3,12 @@ import styled from "styled-components";
 export const StyledSection = styled.section`
   width: 100vw;
   height: 100vh;
-  background-image: url("http://127.0.0.1:5500/src/assets/img/Background-loginpage.svg");
+  
+
+  background-image: url("https://uploaddeimagens.com.br/images/004/384/679/original/back.png?1678453845");
   background-repeat: no-repeat;
   background-size: cover;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -37,13 +40,12 @@ export const StyledSection = styled.section`
     align-items: center;
     justify-content: center;
 
-    gap: 20px;
+    gap: 25px;
   }
 
   h1 {
     color: var(--grey-0);
 
-    padding-bottom: 20px;
 
     font-size: var(--Titles);
     font-weight: 700;
@@ -73,20 +75,40 @@ export const StyledSection = styled.section`
   }
 `;
 
+export const Overlay = styled.div `
+  width: 100vw;
+  height: 100vh;
+  
+  position: absolute;
+  z-index: 1;
+
+
+
+  background-color: rgba(0, 0, 0, 0.8);
+`
+
+
+
 export const StyledDivLoginPage = styled.div`
-  width: 100%;
-  max-width: 500px;
+  width: 85%;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  position: absolute;
+  z-index: 10;
 
   background-color: var(--grey-4);
   border-radius: var(--border-secondary);
 
   @media (min-width: 1000px) {
-    width: 90%;
     max-width: 1200px;
     flex-direction: row;
     justify-content: space-between;
+  }
+
+  label {
+    color: white;
+    margin-bottom: -15px;
   }
 `;
